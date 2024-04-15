955e323148673bf7f6dc8750fd50cf811d2e0391
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-04-15 20:23:37 +0100

    add fly.io url

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,2 +1,2 @@
-export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x8Cab835886c99E3baB74b3D4aB64e86Ae7804d27'
-export const dappRelayAddress = process.env.NODE_ENV === 'development' ? '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE': '0x8Cab835886c99E3baB74b3D4aB64e86Ae7804d27'
+export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x9A4Aacaecf14265c45d728d330976c0BD190c565'
+export const dappRelayAddress = process.env.NODE_ENV === 'development' ? '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE': '0x9A4Aacaecf14265c45d728d330976c0BD190c565'
