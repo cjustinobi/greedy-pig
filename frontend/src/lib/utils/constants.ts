bcf0dbebcf8ffa2f523fad3810467143deab9f5e
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-04-15 22:17:02 +0100

    feat: deploy to fly

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,2 +1,2 @@
-export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x9A4Aacaecf14265c45d728d330976c0BD190c565'
-export const dappRelayAddress = process.env.NODE_ENV === 'development' ? '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE': '0x9A4Aacaecf14265c45d728d330976c0BD190c565'
+export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x7D6bCF9b5bB5Bfb0Ae793082c271931eC333e35D'
+export const dappRelayAddress = process.env.NODE_ENV === 'development' ? '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE': '0x7D6bCF9b5bB5Bfb0Ae793082c271931eC333e35D'
