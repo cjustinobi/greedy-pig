19b6c2e44d1ec881c5e78c2536d499fba6db802c
cjustinobi <justin@Justins-MacBook-Pro.local>
2024-05-17 01:15:53 +0100

    fix: marcus observations

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,3 +1,3 @@
 
 export const dappRelayAddress = '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE'
-export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x5Da0E4A19b2296f2e5c74D79948e2F98f14154FA'
+export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0x9134A7d490bA3270e1E12a968B634dF48DfB77D6'
