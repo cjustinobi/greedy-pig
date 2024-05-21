9417ae5575c8a148d80ef16c9f3090f503b62f1a
cjustinobi <justin@Justins-MacBook-Pro.local>
2024-05-21 11:23:05 +0100

    New build

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,4 +1,4 @@
 
 export const dappRelayAddress = '0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE'
-export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0xc65bf4b414cDb9e7625A33bE0fC993D44030E89a'
+export const dappAddress = process.env.NODE_ENV === 'development' ? '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C' : '0xf39F86487634C23E49038d495F5edc7f5602Ae2E'
 export const userJoiningId = 'jd7047k9yj2h28stf7kj07yk256sfe0b'
