8e8221ad2716b126c8b4519d7f09440ae11c4d70
cjustinobi <justin@Justins-MacBook-Pro.local>
2024-06-15 10:49:00 +0100

    double to single colon

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,91 +35,92 @@ const router = new Router(wallet)
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL
 console.log('HTTP rollup_server url is ' + rollup_server)
 
-let rollup_address = "";
-let advance_req;
+let rollup_address = ''
+let advance_req
 
 
 var handlers = {
   advance_state: handle_advance,
   inspect_state: handle_inspect,
-};
-var finish = { status: "accept" };
+}
+var finish = { status: 'accept' }
 
 const send_request = async (output) => {
   if (output instanceof Output) {
-    let endpoint;
-    console.log("type of output", output.type);
+    let endpoint
+    console.log('type of output', output.type)
 
-    if (output.type == "notice") {
-      endpoint = "/report";
-    } else if (output.type == "voucher") {
-      endpoint = "/voucher";
+    if (output.type == 'notice') {
+      endpoint = '/report'
+    } else if (output.type == 'voucher') {
+      endpoint = '/voucher'
     } else {
-      endpoint = "/report";
+      endpoint = '/report'
     }
 
-    console.log(`sending request ${typeof output}`);
+    console.log(`sending request ${typeof output}`)
     const response = await fetch(rollup_server + endpoint, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(output),
-    });
+    })
     console.debug(
       `received ${output.payload} status ${response.status} body ${JSON.stringify(response.body)}`
-    );
+    )
   }
-};
+}
 
 async function handle_advance(data) {
-  console.log("Received advance request data " + JSON.stringify(data));
+  console.log('Received advance request data ' + JSON.stringify(data))
   try {
-    const payload = data.payload;
-    const msg_sender= data.metadata.msg_sender;
-    console.log("msg sender is", msg_sender.toLowerCase());
-    const payloadStr = hexToString(payload);
+    const payload = data.payload
+    const msg_sender= data.metadata.msg_sender
+    console.log('msg sender is', msg_sender.toLowerCase())
+    const payloadStr = hexToString(payload)
 
     if (msg_sender.toLowerCase() === etherPortalAddress.toLowerCase()) {
       try {
-        return router.process("ether_deposit", payload);
+        return router.process('ether_deposit', payload)
       } catch (e) {
-        return new Error_out(`failed to process ether deposti ${payload} ${e}`);
+        return new Error_out(`failed to process ether deposti ${payload} ${e}`)
       }
     }
     if (msg_sender.toLowerCase() === dappAddressRelay.toLowerCase()) {
-      rollup_address = payload;
-      router.set_rollup_address(rollup_address, "ether_withdraw");
-      router.set_rollup_address(rollup_address, "erc20_withdraw");
-      router.set_rollup_address(rollup_address, "erc721_withdraw");
+      rollup_address = payload
+      router.set_rollup_address(rollup_address, 'ether_withdraw')
+      router.set_rollup_address(rollup_address, 'erc20_withdraw')
+      router.set_rollup_address(rollup_address, 'erc721_withdraw')
+      router.set_rollup_address(rollup_address, 'erc721_withdraw')
 
-      console.log("Setting DApp address");
+      console.log('Setting DApp address')
       return new Notice(
         `DApp address set up successfully to ${rollup_address}`
-      );
+      )
     }
 
     if (msg_sender.toLowerCase() === erc20PortalAddress.toLowerCase()) {
       try {
-        const res = router.process("erc20_deposit", payload);
+        const res = router.process('erc20_deposit', payload)
         console.log('res after depositing erc20 ', res.payload)
-        const payloadStr = hexToString(res.payload);
-        const JSONPayload = JSON.parse(payloadStr);
-        console.log("erc20 payload ", JSONPayload);
+        const payloadStr = hexToString(res.payload)
+        const JSONPayload = JSON.parse(payloadStr)
+        console.log('erc20 payload ', JSONPayload)
         return res
 
       } catch (e) {
-        return new Error_out(`failed to process ERC20Deposit ${payload} ${e}`);
+        return new Error_out(`failed to process ERC20Deposit ${payload} ${e}`)
       }
     }
 
     
-    const JSONPayload = JSON.parse(payloadStr);
-    console.log("payload is ", JSONPayload);
+    const JSONPayload = JSON.parse(payloadStr)
+    console.log('payload is ', JSONPayload)
 
     if (JSONPayload.method === 'createGame') {
       if (JSONPayload.data == '' || null) {
-        console.log('Result cannot be empty');
+        console.log('Result cannot be empty')
         await reportHandler(message)
         return 'reject'
       }
@@ -140,7 +141,7 @@ async function handle_advance(data) {
         if (res.error) {
           return new Error_out(`failed to add participant ${res.message}`)
         }
-        advance_req = await noticeHandler(games);
+        advance_req = await noticeHandler(games)
         return transferNotice
       } catch (error) {
         return new Error_out(`failed to add participant ${error}`)
@@ -154,7 +155,7 @@ async function handle_advance(data) {
         if (JSONPayload.args.to.toLowerCase() !== dappAddress) return new Error_out(`${msg_sender}: You are transfering to the wrong address`)
       }
 
-      console.log('adding participant ...', JSONPayload.data);
+      console.log('adding participant ...', JSONPayload.data)
 
       try {
 
@@ -179,8 +180,8 @@ async function handle_advance(data) {
       console.log('game play ...', JSONPayload.data)
       const res = playGame(JSONPayload.data)
       if (res.error) {
-        await reportHandler(res.message);
-        return 'reject';
+        await reportHandler(res.message)
+        return 'reject'
       }
       advance_req = await noticeHandler(games)
       return 'accept'
@@ -190,8 +191,8 @@ async function handle_advance(data) {
       console.log('rolling dice ...', JSONPayload.data)
       const res = rollDice(JSONPayload.data)
       if (res.error) {
-        await reportHandler(res.message);
-        return 'reject';
+        await reportHandler(res.message)
+        return 'reject'
       }
       advance_req = await noticeHandler(games)
       return 'accept'
@@ -200,8 +201,8 @@ async function handle_advance(data) {
       console.log(`committing for ${msg_sender}...`)
       const res = commit(JSONPayload.gameId, JSONPayload.commitment, msg_sender.toLowerCase())
       if (res.error) {
-        await reportHandler(res.message);
-        return 'reject';
+        await reportHandler(res.message)
+        return 'reject'
       }
      
       advance_req = await noticeHandler(games)
@@ -211,8 +212,8 @@ async function handle_advance(data) {
       console.log(`reveaiing for ${msg_sender} ...`)
       const res = reveal(JSONPayload.gameId, JSONPayload.move, JSONPayload.nonce, msg_sender.toLowerCase())
       if (res.error) {
-        await reportHandler(res.message);
-        return 'reject';
+        await reportHandler(res.message)
+        return 'reject'
       }
      
       advance_req = await noticeHandler(games)
@@ -220,69 +221,69 @@ async function handle_advance(data) {
 
     } else {
 
-      console.log("router process payload ", data)
+      console.log('router process payload ', data)
 
       try {
-        return router.process(JSONPayload.method, data);
+        return router.process(JSONPayload.method, data)
       } catch (e) {
-        return new Error_out(`failed to process command ${payloadStr} ${e}`);
+        return new Error_out(`failed to process command ${payloadStr} ${e}`)
       }
     }
 
     
   } catch (e) {
-    console.error(e);
-    return new Error_out(`failed to process advance_request ${e}`);
+    console.error(e)
+    return new Error_out(`failed to process advance_request ${e}`)
   }
 }
 
 console.log('Game status ', JSON.stringify(games))
 
 async function handle_inspect(data) {
-  console.debug(`received inspect request data${data.payload}`);
+  console.debug(`received inspect request data${data.payload}`)
   try {
-    const url = hexToString(data.payload).split("/");
-    console.log("url is ", url);
-    return router.process(url[0], url[1]);
+    const url = hexToString(data.payload).split('/')
+    console.log('url is ', url)
+    return router.process(url[0], url[1])
   } catch (e) {
-    const error_msg = `failed to process inspect request ${e}`;
-    console.debug(error_msg);
-    return new Error_out(error_msg);
+    const error_msg = `failed to process inspect request ${e}`
+    console.debug(error_msg)
+    return new Error_out(error_msg)
   }
 }
 
 
 (async () => {
   while (true) {
-    const finish_req = await fetch(rollup_server + "/finish", {
-      method: "POST",
+    const finish_req = await fetch(rollup_server + '/finish', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ status: "accept" }),
-    });
+      body: JSON.stringify({ status: 'accept' }),
+    })
 
-    console.log("Received finish status " + finish_req.status);
+    console.log('Received finish status ' + finish_req.status)
 
     if (finish_req.status == 202) {
-      console.log("No pending rollup request, trying again");
+      console.log('No pending rollup request, trying again')
     } else {
-      const rollup_req = await finish_req.json();
+      const rollup_req = await finish_req.json()
 
-      var typeq = rollup_req.request_type;
-      var handler;
-      if (typeq === "inspect_state") {
-        handler = handlers.inspect_state;
+      var typeq = rollup_req.request_type
+      var handler
+      if (typeq === 'inspect_state') {
+        handler = handlers.inspect_state
       } else {
-        handler = handlers.advance_state;
+        handler = handlers.advance_state
       }
-      var output = await handler(rollup_req.data);
-      finish.status = "accept";
+      var output = await handler(rollup_req.data)
+      finish.status = 'accept'
       if (output instanceof Error_out) {
-        finish.status = "reject";
+        finish.status = 'reject'
       }
   
-      await send_request(output);
+      await send_request(output)
     }
   }
-})();
+})()
